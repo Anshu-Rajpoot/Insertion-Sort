0c608c36772cf6a46573f7ee1b57cabc547fe0c0
Anshu Rajpoot <107042248+Anshu-Rajpoot@users.noreply.github.com>
2023-02-15 12:30:35 +0530

    Add files via upload

---
+++ insertionSort.py.py
@@ -0,0 +1,16 @@
+#Insertion Sort
+def Ins(a):
+    for i in range(1,len(a)):
+        key=a[i]
+        j=i-1
+        while j>=0 and key<a[j]:
+            a[j+1]=a[j]
+            j=j-1
+        a[j+1]=key
+data=[]
+n=int(input("How many many Inputs are there : "))
+for i in range(n):
+    x=float(input("Enter the Input number " +str(i+1)+" : "))
+    data.append(x)
+Ins(data)
+print(data)
